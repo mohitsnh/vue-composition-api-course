fee9ca76263e1a3f52933876e58e2f199cb5d1c0
mohitsnh <mohitsnh6@gmail.com>
2023-01-13 15:35:14 +0900

    Added the div inside app.vue

--- vue-composition-api-noteballs/src/App.vue
+++ vue-composition-api-noteballs/src/App.vue
@@ -1,5 +1,10 @@
 <template>
+  <div>
   <router-link to="/">Notes</router-link> | <router-link to="/stats">Stats</router-link> 
+</div>
+<div>
   <router-view></router-view>
+</div>
+  
 </template>
 
