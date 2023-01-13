7411b8e6f0bda0292815d0a633bc182a033cb2ba
mohitsnh <mohitsnh6@gmail.com>
2023-01-13 20:10:31 +0900

    *

--- vue-composition-api-noteballs/src/App.vue
+++ vue-composition-api-noteballs/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <router-link class="button" to="/">Notes</router-link> | <router-link class="button" to="/stats">Stats</router-link> 
+  <NavBar/>
+  
+  <!-- <router-link class="button" to="/">Notes</router-link> | <router-link class="button" to="/stats">Stats</router-link> 
 
-  <router-view></router-view>
+  <router-view></router-view> -->
+
 </template>
-
+<script setup>
+import NavBar from './components/layout/NavBar.vue';
+</script>
 <style>
 @import 'bulma\css\bulma.min.css'
 </style>
